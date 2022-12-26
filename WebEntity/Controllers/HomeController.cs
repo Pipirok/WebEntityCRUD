60be3fc00b6b5ae915205cb2db2e31ecf0ae557c
pipirok <pipirok@vivaldi.net>
2022-12-26 21:12:14 +0400

    Add project files.

---
+++ WebEntity/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebEntity.Models.EntityFrameWork;
+
+namespace WebEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        NorthwindEntities1 db = new NorthwindEntities1();
+        Random r = new Random();
+        public ActionResult Index()
+        {
+            List<WebUser> webUsers = db.WebUsers.ToList();
+            return View(webUsers);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            WebUser randomUser = db.WebUsers.ToList()[r.Next(db.WebUsers.Count() - 1)];
+            return View(randomUser);
+        }
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(string first_name, string last_name, string password)
+        {
+            WebUser userToAdd = new WebUser
+            {
+                username = (first_name + r.Next(100).ToString()).ToLower(),
+                first_name = first_name,
+                last_name = last_name,
+                password = password,
+                register_date = DateTime.Now
+            };
+            db.WebUsers.Add(userToAdd);
+            db.SaveChanges();
+            return Redirect("/Home/Index");
+        }
+
+        [Route("/Home/Delete/{ID}")]
+        public ActionResult Delete(int ID)
+        {
+            WebUser userToDelete = db.WebUsers.Find(ID);
+            if (userToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            db.WebUsers.Remove(userToDelete);
+            db.SaveChanges();
+            return Redirect("/Home/Index");
+        }
+
+        [HttpGet]
+        [Route("/Home/Update/{ID}")]
+        public ActionResult Update(int ID)
+        {
+            WebUser userToUpdate = db.WebUsers.Find(ID);
+            if (userToUpdate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userToUpdate);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int ID, string first_name, string last_name)
+        {
+            WebUser userToUpdate = db.WebUsers.Find(ID);
+            if (userToUpdate == null)
+            {
+                return HttpNotFound();
+            }
+
+            userToUpdate.first_name = first_name;
+            userToUpdate.last_name = last_name;
+            //db.WebUsers.AddOrUpdate(userToUpdate);
+            db.SaveChanges();
+            return Redirect("/Home/Index");
+        }
+    }
+}
---
+++ WebEntity/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebEntity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebEntity")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96cb0657-1a33-4528-ab43-3d5093ebd30c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
